67ba129826ac50382297a72c9f17ab4d41549235
andreeafarcas369 <andreeafarcas369@yahoo.com>
2026-02-05 00:30:44 +0200

    changed the file

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -5,7 +5,7 @@ function Rank({name, entries}) {
     return(
         <>
         <div className="center items-center flex-col mb-2 text-white">
-          <p className="center text-lg w-auto max-w-80">{'Hi, ' + name + ', your rank is: '}</p>
+          <p className="center text-lg w-auto max-w-80">{'Hi, ' + name + ', the total of your entries: '}</p>
           <p className="text-3xl center">{`#${entries}`}</p>
         </div>
         </>
